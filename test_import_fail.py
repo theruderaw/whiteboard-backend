c682a3ee6b6c7604e4f9ff1c4401b5dfa797423a
theruderaw <theruderaw678@gmail.com>
2026-05-12 06:57:28 +0000

    Moveable components

--- test_import_fail.py
+++
@@ -1,6 +0,0 @@
-print("BOOTING TEST...", flush=True)
-try:
-    import asyncpg
-    print("ASYNCPG IMPORTED SUCCESS!", flush=True)
-except Exception as e:
-    print(f"ASYNCPG IMPORT FAILED WITH ERROR: {str(e)}", flush=True)
--- test_import_fail_v2.py
+++
@@ -1,8 +0,0 @@
-print("BOOTING ALL IMPORTS TEST...", flush=True)
-import sys, os, json, asyncio
-print("SYSTEM MODULES IMPORTED", flush=True)
-import asyncpg
-print("ASYNCPG IMPORTED", flush=True)
-from dotenv import load_dotenv
-print("DOTENV IMPORTED", flush=True)
-print("ALL SUCCESSFUL!!!", flush=True)
